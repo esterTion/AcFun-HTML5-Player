fa1d21129632fd1e2e2192b6743fc8a33aebddfe
esterTion <lcz970@126.com>
2017-12-22 10:13:31 +0800

    修复firefox下加载

---
+++ src/ff_fix.js
@@ -0,0 +1,9 @@
+browser.webRequest.onBeforeSendHeaders.addListener(function (d) {
+  for (let i of d.requestHeaders) {
+    if (i.name.toLowerCase() == 'user-agent') i.value = 'yk_acfun;440;Android;7.0;MI 6';
+  }
+  return { requestHeaders: d.requestHeaders };
+},
+  { urls: ['*://video.acfun.cn/*'], types: ['xmlhttprequest'] },
+  ['requestHeaders', 'blocking']
+);
