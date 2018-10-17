0bf27a5a77c98b4c774d9014a5a6cd9f5059b3f9
esterTion <lcz970@126.com>
2018-10-18 00:41:08 +0800

    a修改，应对部分cdn问题 #35

--- src/video_fix.js
+++ src/video_fix.js
@@ -1,9 +1,12 @@
-browser.webRequest.onBeforeSendHeaders.addListener(function (d) {
+chrome.webRequest.onBeforeSendHeaders.addListener(function (d) {
   for (let i of d.requestHeaders) {
     if (i.name.toLowerCase() == 'user-agent') i.value = 'yk_acfun;440;Android;7.0;MI 6';
   }
   return { requestHeaders: d.requestHeaders };
 },
-  { urls: ['*://video.acfun.cn/*'], types: ['xmlhttprequest'] },
+  { urls: [
+    '*://video.acfun.cn/*',
+    '*://*/video.acfun.cn/*'
+  ], types: ['xmlhttprequest'] },
   ['requestHeaders', 'blocking']
 );
